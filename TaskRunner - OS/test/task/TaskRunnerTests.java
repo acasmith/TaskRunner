9a2948df08d3afdf076e869f22ba7f4543a212ee
acasmith <acasmith101@gmail.com>
2021-10-26 20:40:27 +0100

    Added first parallel execution test.

--- TaskRunner - OS/test/task/TaskRunnerTests.java
+++ TaskRunner - OS/test/task/TaskRunnerTests.java
@@ -3,6 +3,9 @@ package task;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
 import static org.mockito.Mockito.*;
 
 import java.util.concurrent.ExecutionException;
@@ -80,6 +83,48 @@ public class TaskRunnerTests {
 		
 		// Act
 		Future<Boolean> pendingResult = taskRunner.runTaskAsync(null, 1, 1, Boolean.class);
+	}
+	
+	@Test
+	public void GivenTwoParallelTasksWhenBothTasksSucceedFirstTimeThenTaskRunnerReturnsCorrectly() throws InterruptedException, ExecutionException {
+		// Arrange
+		TaskRunner taskRunner = new TaskRunner(2);
+		
+		// Task 1 setup
+		ITask<Boolean> mockSuccessfulTask1 = (ITask<Boolean>) mock(ITask.class);
+		when(mockSuccessfulTask1.call(Boolean.class)).thenAnswer(new Answer() {
+			public Boolean answer(InvocationOnMock invocation) throws InterruptedException
+			{
+				System.out.println("mock1 called");
+				Thread.sleep(1000);
+				System.out.println("mock1 completing");
+				return true;
+			}
+		});
+		when(mockSuccessfulTask1.isComplete()).thenReturn(true);
+		
+		// Task 2 Setup
+		ITask<Boolean> mockSuccessfulTask2 = (ITask<Boolean>) mock(ITask.class);
+		when(mockSuccessfulTask2.call(Boolean.class)).thenAnswer(new Answer() {
+			public Boolean answer(InvocationOnMock invocation) throws InterruptedException
+			{
+				System.out.println("mock2 called");
+				Thread.sleep(1000);
+				System.out.println("mock2 completing");
+				return true;
+			}
+		});
+		when(mockSuccessfulTask2.isComplete()).thenReturn(true);
+		
+		
+		// Act
+		Future<Boolean> pendingResult1 = taskRunner.runTaskAsync(mockSuccessfulTask1, 1, 1, Boolean.class);
+		Future<Boolean> pendingResult2 = taskRunner.runTaskAsync(mockSuccessfulTask2, 1, 1, Boolean.class);
+		boolean result1 = pendingResult1.get();
+		boolean result2 = pendingResult2.get();
+		
+		// Assert
+		assertTrue("Both tasks should return true on successful completion. Expected: true, actual: " + (result1 && result2), (result1 && result2));
 	}
 
 }
