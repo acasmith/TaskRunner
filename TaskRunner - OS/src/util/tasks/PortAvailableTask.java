33ec4c361e1ea90c0eea9c0fad91c212a911c0e8
acasmith <acasmith101@gmail.com>
2021-10-26 22:52:09 +0100

    PortAvailabilityTask fully implemented.

--- TaskRunner - OS/src/util/tasks/PortAvailableTask.java
+++ TaskRunner - OS/src/util/tasks/PortAvailableTask.java
@@ -1,24 +1,47 @@
 package util.tasks;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 import task.ITask;
 
 
 public class PortAvailableTask<T> implements ITask<T>{
+	
+	private final int targetPort;
+	private boolean isComplete = false;
 
   public PortAvailableTask(int targetPort) {
-		// TODO Auto-generated constructor stub
+	  if(targetPort < 1 || targetPort > 65535)
+	  {
+		  throw new IllegalArgumentException("Port number out of range. Port number must be in range 0 - 65535 inclusive. Provided value: " + targetPort);
+	  }
+	  this.targetPort = targetPort;
 	}
 
 @Override
   public boolean isComplete() {
-    // TODO Auto-generated method stub
-    return false;
+    return this.isComplete;
   }
 
   @Override
   public T call(Class<T> targetClass) {
-    // TODO Auto-generated method stub
-    return null;
+    try {
+		ServerSocket serverSocket = new ServerSocket(this.targetPort);
+		serverSocket.close();
+		this.setIsComplete();
+		T result = targetClass.cast(true);
+		return result;
+	} catch (IOException e) {
+		e.printStackTrace();
+		T result = targetClass.cast(false);
+		return result;
+	}
+  }
+  
+  private void setIsComplete()
+  {
+	  this.isComplete = true;
   }
 
 }
